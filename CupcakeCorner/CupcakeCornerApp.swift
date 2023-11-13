c2875ee95819ab5204795e72c4f50c89cfaaf6de
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-11-13 09:51:46 -0600

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Oliver Delaney on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
